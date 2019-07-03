84dda72e21f172c699ed2b14a8a9b1f5cf668fe3
evilH2O2 <zelx@ccate.win>
2019-07-03 12:14:44 +0800

    add images

--- install.sh
+++ install.sh
@@ -3,13 +3,8 @@
 [ ${SHELL##/*/} != "zsh" ] && echo '建议 shell使用 Zsh: `chsh -s /usr/bin/zsh`'
 
 echo -e "\033[32m[*]\033[0m (Deploying settings)正在部署设置……\n"
-echo -e "\033[32m[*]\033[0m dotfiles --> $HOME/BackUp\n"
-dir="$HOME/BackUp"
-if [[ ! -d $dir ]]; then  
-	mkdir -p $dir
-fi
+echo -e "\033[32m[*]\033[0m dotfiles --> $HOME/dotfiles\n"
 
-cd $dir
 git clone https://github.com/evilH2O2/dotfiles.git
 cd dotfiles
 sh bin/dotfile.sh
