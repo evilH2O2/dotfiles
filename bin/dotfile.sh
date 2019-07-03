b2b3b4d484f9a0bca43e45e656ed32e8ee967adf
evilH2O2 <zelx@ccate.win>
2019-07-03 21:02:53 +0800

    修改 dotfile.sh

--- bin/dotfile.sh
+++ bin/dotfile.sh
@@ -30,7 +30,6 @@ fileOk(){
 vim(){
 	fileOk "$HOME/.vimrc" "vim/vimrc" ".vimrc"
 
-	# ln -s $HOME/dotfiles/vim/vimrc $HOME/.vimrc
 	ln -s $HOME/dotfiles/vim/color/basepurple.vim /usr/share/vim/vim81/colors/basepurple.vim
 	echo -e "\033[33m[!]\033[0 使用　vim-plug，自动下载(~/.vim)\n"
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -40,28 +39,11 @@ vim(){
 # [rofi]
 rofi(){
 	fileOk "$HOME/.config/rofi" "rofi" ".config/rofi"
-	# ln -s $HOME/dotfiles/rofi      $HOME/.config/rofi
-
-	# ln -s $HOME/dotfiles/rofi/config.rasi      $HOME/.config/rofi/config.rasi
-	# ln -s $HOME/dotfiles/rofi/Light-Theme.rasi $HOME/.config/rofi/Light-Theme.rasi
 }
 
 # [polybar]
 polybar(){
 	fileOk "$HOME/.config/polybar" "polybar" ".config/polybar"
-
-	# ln -s $HOME/dotfiles/polybar/launch.sh               $HOME/.config/polybar/launch.sh
-	# ln -s $HOME/dotfiles/polybar/modules.ini             $HOME/.config/polybar/modules.ini
-	# ln -s $HOME/dotfiles/polybar/bar-backlight.ini       $HOME/.config/polybar/bar-backlight.ini
-	# ln -s $HOME/dotfiles/polybar/bar-keyboard.ini        $HOME/.config/polybar/bar-keyboard.ini
-	# ln -s $HOME/dotfiles/polybar/bar-workspace.ini       $HOME/.config/polybar/bar-workspace.ini
-	# ln -s $HOME/dotfiles/polybar/Script/check-battery.sh $HOME/.config/polybar/Script/check-battery.sh
-	# ln -s $HOME/dotfiles/polybar/Script/check-network.sh $HOME/.config/polybar/Script/check-network.sh
-	# ln -s $HOME/dotfiles/polybar/Script/power.sh         $HOME/.config/polybar/Script/power.sh
-	# ln -s $HOME/dotfiles/polybar/Script/update.sh        $HOME/.config/polybar/Script/update.sh
-	
-	# ln -s $HOME/dotfiles/polybar $HOME/.config/polybar
-	# ln -s $HOME/dotfiles/polybar/launch.sh $HOME/.config/polybar/launch.sh
 }
 
 # [bashrc]
@@ -84,17 +66,12 @@ zsh(){
 # [conky]
 conky(){
 	fileOk "$HOME/.config/conky" "conky" ".config/conky"
-
-	# ln -s $HOME/dotfiles/conky $HOME/.config/conky
-	# ln -s $HOME/dotfiles/conky/conky.conf $HOME/.config/conky/conky.conf
 }
 
 # [i3wm]
 i3wm(){
 	# ln -s $HOME/dotfiles/i3 $HOME/.config/i3
 	fileOk "$HOME/.config/i3" "i3" ".config/i3"
-
-	# ln -s $HOME/dotfiles/i3/config $HOME/.config/i3/config
 }
 
 # [ncmpcpp] 
