53a20bf37582e363b6c1706fd7120665b9fab13e
evilH2O2 <zelx@ccate.win>
2020-06-04 15:32:45 +0800

    update tmux.conf

--- bootrun.sh
+++ bootrun.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
 
 rsync --exclude-from EXCLUDE -avh . ~ \
 	&& source ~/.kalirc
 
-ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/
+if [[ ! -f ~/.config/kitty/kitty.conf ]]; then 
+	ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/
+fi
